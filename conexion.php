b4e3437d554ec67c18f3c7a522a76fc6936d6b2a
JosueEb <e19080804@itmerida.edu.mx>
2023-09-06 10:19:24 -0600

    Conexion a bdd

--- conexion.php
+++ conexion.php
@@ -1,1 +1,12 @@
 <?php
+$host="localhost";
+$user="root";
+$contrasena="12345678";
+$bd="datos";
+
+$conectar=mysqli_connect($host, $user, $contrasena, $bd);
+
+if(!$conectar){
+    echo "No se pudo conectar a la base de datos";
+}
+?>
