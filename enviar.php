f4912393f431fe4e3d474bee8c1c04690392648c
Carolina Domínguez <e19080827@itmerida.edu.mx>
2023-09-06 13:58:26 -0600

    new file:   enviar.php modified:   index.php

---
+++ enviar.php
@@ -0,0 +1,34 @@
+<?php
+  include 'conexion.php';
+
+  $nombre = addslashes($_POST['nombre']);
+  $apellido = addslashes($_POST['apellido']);
+  $fecha = $_POST['fecha'];
+  $telefono = $_POST['telefono'];
+  $correo = $_POST['correo'];
+  $contrasena = $_POST['contra'];
+  $mascotas = $_POST['mascot'];
+  $ip = $_SERVER['REMOTE_ADDR'];
+    echo($ip);
+  $insertar = "INSERT INTO datos (filanombre, filaapellido,filamascota,resultado,numerogenerado,email, contrasena, ip_usuario ) VALUES ('$nombre','$apellido', '$mascotas', '$fecha', '$telefono', '$correo', '$contrasena', '$ip')";
+
+  $query = mysqli_query($conectar,$insertar);
+  if($query){
+    echo '
+    <script>
+    alert("SI SE GUARDARON LOS DATOS CORRECTAMENTE")
+    location.href="index.php"
+    </script>';
+  }
+  else{
+    echo '
+    <script>
+    alert("NO SE GUARDARON LOS DATOS CORRECTAMENTE");
+    window.history.back();
+    </script> ';
+  }
+
+?>
+
+
+>
--- index.php
+++ index.php
@@ -3,28 +3,30 @@ require 'conexion.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
+<head >
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Validación de contraseñas</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
-<body class="ancho">
+<body onload="generarNumero()" class="ancho">
+<form action="enviar.php" id="forml" name="forml" method="POST">    
+
     <div class="contenedor1 ancho">
         <h1>De acuerdo con la información presentada, proporciona un correo electrónico ficticio y una contraseña:</h1>
     </div>
 
     <div class="column">
-        <div id="fila-nombres"name="nombre"></div>
+        <input text="" type="text" id="fila-nombres"name="nombre"/>
     </div>
 
     <div class="column">
-        <div id="fila-apellidos" name="apellido"></div>
+        <input text="" type="text" id="fila-apellidos" name="apellido"/>
     </div>
 
     <div class="column">
         <p>Nombres de tu mascota:</p>
-        <div id="fila-mascotas" name="mascotas"></div>
+        <input text="" type="text" id="fila-mascotas" name="mascot"/>
     </div>
 
     <div style="clear: both;"></div> <!-- Clear float para evitar problemas de altura -->
@@ -33,60 +35,68 @@ require 'conexion.php';
         <h1>Edad</h1>
         <label for="fecha-nacimiento">Fecha de Nacimiento:</label>
         <input type="date" id="fecha-nacimiento">
-        <button onclick="calcularEdad()">Calcular Edad</button>
-        <p id="resultado"></p>
+        <input class="btn_enviar" onclick="calcularEdad()" value="Guardar" type="button"/>
+        <input type="text" id="resultado" name="fecha" readonly="true"/>
     </div>
 
     <div>
-        <h1>Generador de Números de Teléfono</h1>
-        <label for="lada">Selecciona una Lada:</label>
+        <h1>Números de Teléfono</h1>
+        <!--<label for="lada">Selecciona una Lada:</label>
         <select id="lada">
             <option value="999">999</option>
             <option value="988">988</option>
-            <!-- Agrega más opciones de ladas según sea necesario -->
-        </select>
-        <button onclick="generarNumero()">Generar Número</button>
-        <p id="numero-generado"></p>
+            Agrega más opciones de ladas según sea necesario
+        </select
+        <button-- onclick="generarNumero()">Generar Número</button-->
+        <input type="text" id="numero-generado" name="telefono"/>
     </div>
 
     <div>
         <label for="correo">Ingresa un correo electrónico Ficticio</label><br>
-        <input type="text" id="email" placeholder="Correo Electrónico" size="30">
+        <input type="text" id="email" placeholder="Correo Electrónico" size="30" name="correo">
         <br>
         <label for="contrasena">Ingresa una Contraseña:</label><br>
-        <input type="text" id="contrasena" placeholder="Contraseña" size="30"><br>
-        <button onclick="guardarTexto()">Guardar</button>
+        <input type="text" id="contrasena" placeholder="Contraseña" size="30" name="contra"><br>
+        <input class="btn_enviar" type="button" value="Enviar" onclick="validar_formulario()"/>
     </div>
+
+</form>
 
     <script>
         // Función para cargar y mostrar un elemento aleatorio de un archivo CSV
         function mostrarElementoAleatorio(url, containerId) {
-            var container = document.getElementById(containerId);
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', url, true);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
 
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    var csvData = xhr.responseText;
-                    var rows = csvData.split('\n');
-                    var randomRowIndex = Math.floor(Math.random() * (rows.length - 1)) + 1; // Selecciona una fila aleatoria (excluyendo encabezados)
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var csvData = xhr.responseText;
+            var rows = csvData.split('\n');
+            var randomRowIndex = Math.floor(Math.random() * (rows.length - 1)) + 1; // Selecciona una fila aleatoria (excluyendo encabezados)
 
-                    var cells = rows[randomRowIndex].split(',');
-                    var tableHtml = '<table>';
-                    tableHtml += '<tr>';
-                    for (var j = 0; j < cells.length; j++) {
-                        tableHtml += '<td>' + cells[j] + '</td>';
-                    }
-                    tableHtml += '</tr>';
-                    tableHtml += '</table>';
-                    container.innerHTML = tableHtml;
-                } else {
-                    console.error('No se pudo cargar el archivo CSV.');
-                }
+            var cells = rows[randomRowIndex].split(',');
+
+            // Crea un objeto con los datos del elemento aleatorio
+            var elementoAleatorio = {
+                nombre: cells[0],
+                apellido: cells[1],
+                // Agrega más propiedades según sea necesario
             };
 
-            xhr.send();
+            // Ahora puedes usar este objeto en otras partes del código
+            var campoNombre = document.getElementById(containerId);
+            // alert (elementoAleatorio.nombre);
+            campoNombre.value = elementoAleatorio.nombre;
+
+            // También puedes almacenar el objeto en una variable global si lo necesitas en múltiples lugares.
+            // window.elementoAleatorio = elementoAleatorio;
+        } else {
+            console.error('No se pudo cargar el archivo CSV.');
         }
+    };
+
+    xhr.send();
+}
 
         // Llama a la función para mostrar un elemento aleatorio del archivo "mujeres.csv"
         mostrarElementoAleatorio('data/sep/nombres.csv', 'fila-nombres');
@@ -105,24 +115,39 @@ require 'conexion.php';
             // Calcula la edad en años
             const edad = Math.floor(edadMilisegundos / (365 * 24 * 60 * 60 * 1000));
 
-            document.getElementById("resultado").innerText = `Tienes ${edad} años.`;
+            document.getElementById("resultado").value = `${edad}`;
         }
         function generarNumero() {
-            const lada = document.getElementById("lada").value;
+            // const lada = document.getElementById("lada").value;
             
             // Genera los últimos 6 dígitos del número aleatoriamente
             const numerosAleatorios = Math.floor(Math.random() * 1000000).toString().padStart(7, '0');
             
-            const numero = `${lada} ${numerosAleatorios}`;
+            const numero = `999 ${numerosAleatorios}`;
             
-            document.getElementById("numero-generado").innerText = `Número de teléfono generado: ${numero}`;
+            document.getElementById("numero-generado").value = `${numero}`;
         }
-        //Guardar los cuadros de texto
-        function guardarTextos() {
-            const correo = document.getElementById("correo").value;
-            const contrasena = document.getElementById("contrasena").value;
 
-            document.getElementById("textosGuardados").innerText = `correo: ${correo}, contrasena: ${contrasena}`;
+        //Guardar los cuadros de texto
+        function validar_formulario(){
+            if (document.getElementById('email').value.length == 0) {
+                alert("Escribe el correo electronico")
+                document.getElementById('email').focus()
+                return 0
+            }
+            if (document.getElementById('contrasena').value.length == 0) {
+                alert("Escribe la contraseña ")
+                document.getElementById('contrasena').focus()
+                return 0
+            }
+            if (document.getElementById('resultado').value.length == 0) {
+                alert("Calcula la edad")
+                document.getElementById('resultado').focus()
+                return 0
+            }
+
+
+            document.forml.submit()
         }
     </script>
 </body>
